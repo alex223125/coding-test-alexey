094f020bc3d16e3e46220776b001c6ba5597a12e
Alexey Sologub <alexeysologub51@gmail.com>
2022-09-25 20:41:54 -0400

    Finding entty added

---
+++ backend/app/models/finding.rb
@@ -0,0 +1,28 @@
+class Finding < ApplicationRecord
+  store_accessor :schema, :required, :attributes
+  serialize :description
+
+  after_initialize :define_default_values
+
+  private
+
+  def define_default_values
+    binding.pry
+    return unless self.description.blank?
+
+    binding.pry
+    self.description = self.schema['attributes'].keys.reduce({}) do |acc, field|
+      acc[field] = self.schema['attributes'][field]['default']
+      acc
+    end
+  end
+
+  def validate_from_schema
+    self.schema['required'].each do |field|
+      errors.add(field, 'is required') if self.description[field].blank?
+    end
+  end
+
+end
+
+
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_220956) do
+ActiveRecord::Schema.define(version: 2022_09_25_231042) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,9 +38,16 @@ ActiveRecord::Schema.define(version: 2022_09_25_220956) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "findings", force: :cascade do |t|
+    t.jsonb "schema", default: {"required"=>["plugin_name", "port", "severity", "plugin_id", "data"], "attributes"=>{"data"=>{"type"=>"text", "default"=>""}, "port"=>{"type"=>"string", "default"=>""}, "severity"=>{"type"=>"string", "default"=>""}, "plugin_id"=>{"type"=>"integer", "default"=>nil}, "plugin_name"=>{"type"=>"string", "default"=>""}}}, null: false
+    t.jsonb "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reports", force: :cascade do |t|
