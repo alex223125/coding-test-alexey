1201e90eea1ab4e77c0858ccb66264db59fdac82
Alexey Sologub <alexeysologub51@gmail.com>
2022-09-28 20:56:45 -0400

    Service for downloading report data rewritten, now we will store chunks of xml in table Segments

--- backend/app/services/reports/DownloadReport.rb
+++ backend/app/services/reports/DownloadReport.rb
@@ -2,37 +2,79 @@ module Services
   module Reports
     class DownloadReport
 
+      REPORT_ITEM_CATEGORY = "NessusReportItem"
+
       def initialize(report)
         @report = report
+        @rd, @wr = IO.pipe
       end
 
       def call
-        download
-        attach if @downloaded_file.present?
-        mark_as_downloaded if @downloaded_file.present?
-        mark_as_invalid if @error_reason.present?
+        read_request
+        ActiveRecord::Base.transaction do
+          pars_chunks
+          close_threads
+          mark_as_downloaded
+        end
+      rescue ActiveRecord::RecordInvalid => exception
+        message = exception.message
+        Rails.logger.error(message)
+        mark_as_invalid(message)
       end
 
       private
 
-      def download
-        @downloaded_file = Down.download(@report.link)
-      rescue Down::InvalidUrl => e
-        @error_reason = e.message
-        Rails.logger.error(@error_reason)
+      def request
+        @request = Net::HTTP::Get.new(uri.request_uri)
       end
 
-      def attach
-        @report.file.attach(io: @downloaded_file, filename: "File attachment for: #{@report.title}")
+      def uri
+        @uri ||= URI(@report.link)
+      end
+
+      def read_request
+        @reader_thread = Thread.new do
+          Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+            http.request(request) do |response|
+              response.read_body do |chunk|
+                @wr.write(chunk)
+              end
+            end
+            @wr.close
+          end
+        end
+      end
+
+      def pars_chunks
+        reader = Nokogiri::XML::Reader(@rd)
+        reader.each do |node|
+          next if node.node_type != Nokogiri::XML::Reader::TYPE_ELEMENT
+          if node.name == "ReportItem"
+            create_segment(node)
+          end
+        end
+        @rd.close
+      end
+
+      def create_segment(node)
+        report_item = Segment.new
+        report_item.report = @report
+        report_item.category = REPORT_ITEM_CATEGORY
+        report_item.content = Nokogiri::XML(node.outer_xml)
+        report_item.save!
+      end
+
+      def close_threads
+        @reader_thread.join
       end
 
       def mark_as_downloaded
         @report.is_downloaded = true
-        @report.save
+        @report.save!
       end
 
-      def mark_as_invalid
-        @report.error_reason = @error_reason
+      def mark_as_invalid(e)
+        @report.error_reason = e
         @report.is_url_valid = false
         @report.save
       end
