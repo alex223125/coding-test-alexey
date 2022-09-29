5f87590d94495e8cd6b8d32698bc83d804e387df
Alexey Sologub <alexeysologub51@gmail.com>
2022-09-28 21:39:50 -0400

    sidekiq gem added + schduler for background processing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :segments
   resources :findings
   resources :reports
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  Rails.application.routes.draw do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
