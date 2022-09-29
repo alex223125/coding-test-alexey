8de230ce2fabac7595b1b25b25b04af9308240c6
Alexey Sologub <alexeysologub51@gmail.com>
2022-09-28 21:25:45 -0400

    show and index action for finding api added

---
+++ backend/app/controllers/findings_controller.rb
@@ -0,0 +1,20 @@
+class FindingsController < ApplicationController
+  before_action :set_finding, only: [:show]
+
+  # GET /findings
+  def index
+    @findings = Finding.all
+    render json: @findings, each_serializer: FindingSerializer
+  end
+
+  # GET /findings/1
+  def show
+    render json: @finding, serializer: FindingSerializer, status: :ok, message: 'Success'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_finding
+      @finding = Finding.find(params[:id])
+    end
+end
